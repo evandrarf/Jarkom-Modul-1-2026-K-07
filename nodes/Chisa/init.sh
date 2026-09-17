636061c7e20bdd64c46b62ee459ee4584d49e3bd
Evandra Raditya <evandraraditya049@gmail.com>
2026-09-17 23:03:55 +0700

    feat: add node init file

---
+++ nodes/Chisa/init.sh
@@ -0,0 +1,3 @@
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+/root/copy_config_ftp.sh
+/root/setup_ftp.sh
---
+++ nodes/Lain/init.sh
@@ -0,0 +1,3 @@
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sysctl -w net.ipv4.ip_forward=1
+./cek_status.sh
